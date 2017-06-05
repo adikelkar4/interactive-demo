a6a89f6d68553d8e1adbccfb209ac10374392f2c
Kevin White <kwhite@nuodb.com>
2017-06-05 16:08:32 -0400

    [OA-2122] Created StorefrontUser JAR

--- StorefrontUser/src/main/java/com/nuodb/storefront/StorefrontApp.java
+++ StorefrontUser/src/main/java/com/nuodb/storefront/StorefrontApp.java
@@ -2,245 +2,152 @@
 
 package com.nuodb.storefront;
 
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.codehaus.jettison.json.JSONObject;
 
-import com.nuodb.storefront.model.dto.ProductFilter;
 import com.nuodb.storefront.model.dto.Workload;
-import com.nuodb.storefront.model.dto.WorkloadStats;
 import com.nuodb.storefront.model.dto.WorkloadStep;
-import com.nuodb.storefront.model.dto.WorkloadStepStats;
-import com.nuodb.storefront.model.entity.Product;
-import com.nuodb.storefront.service.IDataGeneratorService;
 import com.nuodb.storefront.service.ISimulatorService;
-import com.nuodb.storefront.service.IStorefrontService;
 import com.nuodb.storefront.service.IStorefrontTenant;
-import com.nuodb.storefront.util.DDLUtil;
 
 public class StorefrontApp {
-    static {
-        // Try applying properties from specified file
-        try {
-            String propertyFile = System.getProperty("properties", null);
-            if (propertyFile != null) {
-                Properties overrides = new Properties();
-                overrides.load(new FileInputStream(propertyFile));
-                System.getProperties().putAll(overrides);
-            }
-        } catch (Exception e) {
-            Logger.getLogger(StorefrontApp.class).warn("Failed to read properties file", e);
-        }
-        
-        //  For JSP page compilation, use Jetty compiler when available to avoid JDK dependency
-        System.setProperty("org.apache.jasper.compiler.disablejsr199", "true");        
-    }
-    
-    // Storefront properties
-    public static final String IP_DETECT_URL = System.getProperty("storefront.ipDetectUrl", "http://checkip.amazonaws.com");
-    public static final String DEFAULT_URL = System.getProperty("storefront.url", "{protocol}://{host}:{port}/{context}");
-    public static final int DEFAULT_PORT = Integer.valueOf(System.getProperty("maven.tomcat.port", "9001"));
-    public static final String APP_NAME = System.getProperty("storefront.name", "NuoDB Storefront Demo");
-    public static final String TENANT_PARAM_NAME = "tenant"; 
-    public static final String LOGGER_NAME_TENANT_SEP = ":";
-    
-    // Storefront timings
-    public static final int HEARTBEAT_INTERVAL_SEC = 10;
-    public static final int CPU_SAMPLING_INTERVAL_SEC = 1;
-    public static final int MAX_HEARTBEAT_AGE_SEC = 20;
-    public static final int PURGE_FREQUENCY_SEC = 60 * 30; // 30 min
-    public static final int STOP_USERS_AFTER_IDLE_UI_SEC = 60 * 10; // 10 min
-    public static final int MIN_INSTANCE_PURGE_AGE_SEC = 60 * 60; // 1 hour
-    public static final int DEFAULT_SESSION_TIMEOUT_SEC = 60 * 20;// 20 min
-    public static final int DEFAULT_ANALYTIC_MAX_AGE = 60 * 30;// 30 min
-    public static final int BENCHMARK_DURATION_MS = 10000;
-    public static final int SIMULATOR_STATS_DISPLAY_INTERVAL_MS = 5000;
-    public static final int GC_CUMULATIVE_TIME_LOG_MS = 500; // every 0.5 sec of cumulative GC time logged
+	static {
+		// Try applying properties from specified file
+		try {
+			String propertyFile = System.getProperty("properties", null);
+			if (propertyFile != null) {
+				Properties overrides = new Properties();
+				overrides.load(new FileInputStream(propertyFile));
+				System.getProperties().putAll(overrides);
+			}
+		} catch (Exception e) {
+			Logger.getLogger(StorefrontApp.class).warn("Failed to read properties file", e);
+		}
 
-    // Database properties
-    public static final String DB_NAME = System.getProperty("storefront.db.name");
-    public static final String DB_USER = System.getProperty("storefront.db.user");
-    public static final String DB_PASSWORD = System.getProperty("storefront.db.password");
-    public static final String DB_OPTIONS = System.getProperty("storefront.db.options");
-    public static final int DB_PING_TIMEOUT_SEC = Integer.valueOf(System.getProperty("storefront.db.pingTimeoutSec", "0")); // 0=disabled
-    public static final int DB_MAX_INIT_WAIT_TIME_SEC = Integer.valueOf(System.getProperty("storefront.db.initWaitTimeSec", "5"));
-    public static final String DB_DOMAIN_BROKER =System.getProperty("domain.broker", "localhost");
-    public static final String DB_PROCESS_TAG = System.getProperty("storefront.db.processTag", "demo_${db.name}");
-    
-    // Database API properties
-    public static final int DBAPI_READ_TIMEOUT_SEC = Integer.valueOf(System.getProperty("storefront.dbapi.readTimeoutSec", "10"));
-    public static final int DBAPI_CONNECT_TIMEOUT_SEC = Integer.valueOf(System.getProperty("storefront.dbapi.connectTimeoutSec", "10"));
-    public static final int DBAPI_MAX_UNAVAILABLE_RETRY_TIME_SEC = Integer.valueOf(System.getProperty("storefront.dbapi.unavailableRetryTimeSec", "3"));
-    public static final String DBAPI_HOST = System.getProperty("storefront.dbapi.host");
-    public static final int DBAPI_PORT = Integer.valueOf(System.getProperty("storefront.dbapi.port", "8888"));
-    public static final String DBAPI_USERNAME = System.getProperty("storefront.dbapi.user", "domain");
-    public static final String DBAPI_PASSWORD = System.getProperty("storefront.dbapi.password", "bird");
-    
-    // Other properties
-    public static final int SQLEXPLORER_PORT = Integer.valueOf(System.getProperty("storefront.sqlexplorer.port", "9001"));
+		// For JSP page compilation, use Jetty compiler when available to avoid
+		// JDK dependency
+		System.setProperty("org.apache.jasper.compiler.disablejsr199", "true");
+	}
 
-    // Default names
-    public static final String DEFAULT_DB_NAME = "Storefront";
-    public static final String DEFAULT_DB_HOST = "localhost";
-    public static final String DEFAULT_REGION_NAME = "Unknown region";
-    public static final String DEFAULT_TENANT_NAME = "Default";
-    
-    /**
-     * Command line utility to perform various actions related to the Storefront application.
-     * 
-     * Specify each action as a separate argument. Valid actions are:
-     * <ul>
-     * <li>create -- create schema</li>
-     * <li>drop -- drop schema</li>
-     * <li>showddl -- display drop and create DDL</li>
-     * <li>generate -- generate dummy storefront data</li>
-     * <li>load -- load storefront data from src/main/resources/sample-products.json file</li>
-     * <li>simulate -- simulate customer activity</li>
-     * <li>benchmark -- run benchmark workload</li>
-     * </ul>
-     */
-    public static void main(String[] args) throws Exception {
-        IStorefrontTenant tenant = StorefrontTenantManager.getDefaultTenant();
-        
-        for (int i = 0; i < args.length; i++) {
-            String action = args[i];
-            if ("create".equalsIgnoreCase(action)) {
-                createSchema(tenant.createSchemaExport());
-                System.out.println("Tables created successfully.");
-            } else if ("drop".equalsIgnoreCase(action)) {
-                dropSchema(tenant.createSchemaExport());
-                System.out.println("Tables dropped successfully.");
-            } else if ("showddl".equalsIgnoreCase(action)) {
-                showDdl(tenant.createSchemaExport());
-            } else if ("generate".equalsIgnoreCase(action)) {
-                System.out.println("Generating data...");
-                generateData(tenant.createDataGeneratorService());
-                System.out.println("Data generated successfully.  " + getProductStats(tenant.createStorefrontService()));
-            } else if ("load".equalsIgnoreCase(action)) {
-                System.out.println("Loading data...");
-                loadData(tenant.createDataGeneratorService());
-                System.out.println("Data loaded successfully.  " + getProductStats(tenant.createStorefrontService()));
-            } else if ("simulate".equalsIgnoreCase(action)) {
-                simulateActivity(tenant.getSimulatorService());
-            } else if ("benchmark".equalsIgnoreCase(action)) {
-                benchmark(tenant.getSimulatorService());
-            } else {
-                throw new IllegalArgumentException("Unknown action:  " + action);
-            }
-        }
-    }
+	// Storefront properties
+	public static final String IP_DETECT_URL = System.getProperty("storefront.ipDetectUrl",
+			"http://checkip.amazonaws.com");
+	public static final String DEFAULT_URL = System.getProperty("storefront.url",
+			"{protocol}://{host}:{port}/{context}");
+	public static final int DEFAULT_PORT = Integer.valueOf(System.getProperty("maven.tomcat.port", "9001"));
+	public static final String APP_NAME = System.getProperty("storefront.name", "NuoDB Storefront Demo");
+	public static final String TENANT_PARAM_NAME = "tenant";
+	public static final String LOGGER_NAME_TENANT_SEP = ":";
 
-    protected static String getProductStats(IStorefrontService svc) {
-        ProductFilter filter = new ProductFilter();
-        filter.setPageSize(null);
-        int numProducts = svc.getProducts(filter).getTotalCount();
-        int numCategories = svc.getCategories().getTotalCount();
-        return "There are now " + numProducts + " products across " + numCategories + " categories.";
-    }
+	// Storefront timings
+	public static final int HEARTBEAT_INTERVAL_SEC = 10;
+	public static final int CPU_SAMPLING_INTERVAL_SEC = 1;
+	public static final int MAX_HEARTBEAT_AGE_SEC = 20;
+	public static final int PURGE_FREQUENCY_SEC = 60 * 30; // 30 min
+	public static final int STOP_USERS_AFTER_IDLE_UI_SEC = 60 * 10; // 10 min
+	public static final int MIN_INSTANCE_PURGE_AGE_SEC = 60 * 60; // 1 hour
+	public static final int DEFAULT_SESSION_TIMEOUT_SEC = 60 * 20;// 20 min
+	public static final int DEFAULT_ANALYTIC_MAX_AGE = 60 * 30;// 30 min
+	public static final int BENCHMARK_DURATION_MS = 10000;
+	public static final int SIMULATOR_STATS_DISPLAY_INTERVAL_MS = 5000;
+	public static final int GC_CUMULATIVE_TIME_LOG_MS = 500; // every 0.5 sec of
+																// cumulative GC
+																// time logged
 
-    public static void createSchema(SchemaExport export) {
-        export.create(false, true);
-    }
+	// Database properties
+	public static final String DB_NAME = System.getProperty("storefront.db.name");
+	public static final String DB_USER = System.getProperty("storefront.db.user");
+	public static final String DB_PASSWORD = System.getProperty("storefront.db.password");
+	public static final String DB_OPTIONS = System.getProperty("storefront.db.options");
+	public static final int DB_PING_TIMEOUT_SEC = Integer
+			.valueOf(System.getProperty("storefront.db.pingTimeoutSec", "0")); // 0=disabled
+	public static final int DB_MAX_INIT_WAIT_TIME_SEC = Integer
+			.valueOf(System.getProperty("storefront.db.initWaitTimeSec", "5"));
+	public static final String DB_DOMAIN_BROKER = System.getProperty("domain.broker", "localhost");
+	public static final String DB_PROCESS_TAG = System.getProperty("storefront.db.processTag", "demo_${db.name}");
 
-    public static void dropSchema(SchemaExport export) {
-        export.drop(false, true);
-    }
+	// Database API properties
+	public static final int DBAPI_READ_TIMEOUT_SEC = Integer
+			.valueOf(System.getProperty("storefront.dbapi.readTimeoutSec", "10"));
+	public static final int DBAPI_CONNECT_TIMEOUT_SEC = Integer
+			.valueOf(System.getProperty("storefront.dbapi.connectTimeoutSec", "10"));
+	public static final int DBAPI_MAX_UNAVAILABLE_RETRY_TIME_SEC = Integer
+			.valueOf(System.getProperty("storefront.dbapi.unavailableRetryTimeSec", "3"));
+	public static final String DBAPI_HOST = System.getProperty("storefront.dbapi.host");
+	public static final int DBAPI_PORT = Integer.valueOf(System.getProperty("storefront.dbapi.port", "8888"));
+	public static final String DBAPI_USERNAME = System.getProperty("storefront.dbapi.user", "domain");
+	public static final String DBAPI_PASSWORD = System.getProperty("storefront.dbapi.password", "bird");
 
-    public static void showDdl(SchemaExport export) {
-        System.out.println(DDLUtil.generateAndFormat(export));
-    }
+	// Other properties
+	public static final int SQLEXPLORER_PORT = Integer
+			.valueOf(System.getProperty("storefront.sqlexplorer.port", "9001"));
 
-    public static void generateData(IDataGeneratorService svc) throws IOException {
-        try {
-            svc.generateAll(100, 5000, 2, 10);
-        } finally {
-            svc.close();
-        }
-    }
+	// Default names
+	public static final String DEFAULT_DB_NAME = "Storefront";
+	public static final String DEFAULT_DB_HOST = "localhost";
+	public static final String DEFAULT_REGION_NAME = "Unknown region";
+	public static final String DEFAULT_TENANT_NAME = "Default";
 
-    public static void removeData(IDataGeneratorService svc) throws IOException {
-        try {
-            svc.removeAll();
-        } finally {
-            svc.close();
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		List<String> argsList = Arrays.asList(args);
+		List<String> dbArgs = argsList.stream().filter(string -> string.contains("db.")).collect(Collectors.toList());
+		List<String> appArgs = argsList.stream().filter(string -> string.contains("app.")).collect(Collectors.toList());
+		List<String> workloadArgs = argsList.stream().filter(string -> string.contains("workload."))
+				.collect(Collectors.toList());
+		Map<String, String> dbSettings = new HashMap<>();
+		Map<String, String> workloadSettings = new HashMap<>();
+		Map<String, String> appSettings = new HashMap<>();
 
-    public static void loadData(IDataGeneratorService svc) throws IOException {
-        InputStream stream = StorefrontApp.class.getClassLoader().getResourceAsStream("sample-products.json");
-        ObjectMapper mapper = new ObjectMapper();
+		dbArgs.forEach(setting -> dbSettings.put(setting.split("=")[0], setting.split("=")[1]));
+		workloadArgs.forEach(setting -> workloadSettings.put(setting.split("=")[0], setting.split("=")[1]));
+		appArgs.forEach(setting -> appSettings.put(setting.split("=")[0], setting.split("=")[1]));
+		IStorefrontTenant tenant = StorefrontTenantManager.createTenant(dbSettings.get("db.name"), dbSettings);
+		executeTasks(tenant.getSimulatorService(), workloadSettings);
+		postStatsAsJson(tenant, appSettings, tenant.getTransactionStats());
+		postStatsAsJson(tenant, appSettings, tenant.getSimulatorService().getWorkloadStats());
+	}
 
-        // Read products from JSON file
-        List<Product> products = mapper.readValue(stream, new TypeReference<ArrayList<Product>>() {
-        });
+	@SuppressWarnings("rawtypes")
+	private static void postStatsAsJson(IStorefrontTenant tenant, Map<String, String> appSettings, Map stats)
+			throws MalformedURLException, IOException, ProtocolException {
+		HttpURLConnection connection = buildStatsConnection(appSettings, "POST");
+		DataOutputStream stream = new DataOutputStream(connection.getOutputStream());
+		JSONObject object = new JSONObject(stats);
+		stream.writeBytes(object.toString());
+		stream.flush();
+		stream.close();
+	}
 
-        // Load products into DB, and load generated views
-        try {
-            svc.generateProductReviews(100, products, 10);
-        } finally {
-            svc.close();
-        }
-    }
+	private static HttpURLConnection buildStatsConnection(Map<String, String> appSettings, String method)
+			throws MalformedURLException, IOException, ProtocolException {
+		URL url = new URL(appSettings.get("app.url"));
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod(method);
+		connection.setRequestProperty("Content-Type", "application/json");
+		connection.setDoOutput(true);
+		return connection;
+	}
 
-    public static void benchmark(ISimulatorService simulator) throws InterruptedException {
-        Workload shoppersWithNoWait = new Workload("Customer:  Instant purchaser", true, 0, 0, Workload.DEFAULT_MAX_WORKERS, WorkloadStep.MULTI_SHOP);
-
-        simulator.addWorkers(shoppersWithNoWait, 100, 0);
-        Thread.sleep(BENCHMARK_DURATION_MS);
-        System.out.println(simulator.getWorkloadStats().get(shoppersWithNoWait.getName()).getWorkCompletionCount());
-        simulator.removeAll();
-    }
-
-    public static void simulateActivity(ISimulatorService simulator) throws InterruptedException {
-        simulator.adjustWorkers(Workload.BROWSER, 20, 25);
-        simulator.addWorkers(Workload.BROWSER, 20, 250);
-        simulator.addWorkers(Workload.SHOPPER_FAST, 20, 250);
-        simulator.addWorkers(Workload.REVIEWER, 20, 250);
-
-        for (int i = 0; i < 20; i++) {
-            printSimulatorStats(simulator, System.out);
-            Thread.sleep(SIMULATOR_STATS_DISPLAY_INTERVAL_MS);
-        }
-        printSimulatorStats(simulator, System.out);
-        simulator.removeAll();
-    }
-
-    private static void printSimulatorStats(ISimulatorService simulator, PrintStream out) {
-        out.println();
-        out.println(String.format("%-30s %8s %8s %8s %8s | %7s %9s %7s %9s", "Workload", "Active", "Failed", "Killed", "Complete", "Steps",
-                "Avg (s)", "Work", "Avg (s)"));
-        for (Map.Entry<String, WorkloadStats> statsEntry : simulator.getWorkloadStats().entrySet()) {
-            String workloadName = statsEntry.getKey();
-            WorkloadStats stats = statsEntry.getValue();
-
-            out.println(String.format("%-30s %8d %8d %8d %8d | %7d %9.3f %7d %9.3f",
-                    workloadName,
-                    stats.getActiveWorkerCount(),
-                    stats.getFailedWorkerCount(),
-                    stats.getKilledWorkerCount(),
-                    stats.getCompletedWorkerCount(),
-                    stats.getWorkInvocationCount(),
-                    (stats.getAvgWorkTimeMs() != null) ? stats.getAvgWorkTimeMs() / 1000f : null,
-                    stats.getWorkCompletionCount(),
-                    (stats.getAvgWorkCompletionTimeMs() != null) ? stats.getAvgWorkCompletionTimeMs() / 1000f : null));
-        }
-
-        out.println();
-        out.println(String.format("%-25s %20s", "Step:", "# Completions:"));
-        for (Map.Entry<WorkloadStep, WorkloadStepStats> statsEntry : simulator.getWorkloadStepStats().entrySet()) {
-            WorkloadStep step = statsEntry.getKey();
-            WorkloadStepStats stats = statsEntry.getValue();
-            out.println(String.format("%-25s %20d", step.name(), stats.getCompletionCount()));
-        }
-    }
+	public static void executeTasks(ISimulatorService simulator, Map<String, String> workloadSettings)
+			throws InterruptedException {
+		for (String setting : workloadSettings.keySet()) {
+			WorkloadStep step = WorkloadStep.valueOf(setting.split(".")[1]);
+			simulator.addWorkers(new Workload(step.name() + "-" + Long.toString(System.currentTimeMillis()), true, 2000,
+					1500, Workload.DEFAULT_MAX_WORKERS, step), Integer.parseInt(workloadSettings.get(setting)), 25);
+		}
+	}
 }
--- StorefrontUser/src/main/java/com/nuodb/storefront/StorefrontTenantManager.java
+++ StorefrontUser/src/main/java/com/nuodb/storefront/StorefrontTenantManager.java
@@ -21,100 +21,125 @@ import com.nuodb.storefront.service.IStorefrontTenant;
 import com.nuodb.storefront.service.storefront.StorefrontTenant;
 
 public class StorefrontTenantManager {
-    private static final AppInstance s_defaultAppInstance =
-            new AppInstance(StorefrontApp.DEFAULT_REGION_NAME, StorefrontApp.DEFAULT_TENANT_NAME, true);
-    private static final IStorefrontTenant s_defaultTenant = new StorefrontTenant(s_defaultAppInstance);
-    private static final Map<String, IStorefrontTenant> s_tenantMap = new TreeMap<String, IStorefrontTenant>(String.CASE_INSENSITIVE_ORDER);
+	private static final AppInstance s_defaultAppInstance = new AppInstance(StorefrontApp.DEFAULT_REGION_NAME,
+			StorefrontApp.DEFAULT_TENANT_NAME, true);
+	private static final IStorefrontTenant s_defaultTenant = new StorefrontTenant(s_defaultAppInstance);
+	private static final Map<String, IStorefrontTenant> s_tenantMap = new TreeMap<String, IStorefrontTenant>(
+			String.CASE_INSENSITIVE_ORDER);
 
-    static {
-        s_tenantMap.put(StorefrontApp.DEFAULT_TENANT_NAME, s_defaultTenant);
-    }
+	static {
+		s_tenantMap.put(StorefrontApp.DEFAULT_TENANT_NAME, s_defaultTenant);
+	}
 
-    public static IStorefrontTenant getDefaultTenant() {
-        return s_defaultTenant;
-    }
+	public static IStorefrontTenant getDefaultTenant() {
+		return s_defaultTenant;
+	}
 
-    public static IStorefrontTenant getTenant(HttpServletRequest request) {
-        return getTenant(request.getParameter(StorefrontApp.TENANT_PARAM_NAME));
-    }
+	public static IStorefrontTenant getTenant(HttpServletRequest request) {
+		return getTenant(request.getParameter(StorefrontApp.TENANT_PARAM_NAME));
+	}
 
-    public static IStorefrontTenant getTenantOrDefault(String tenantName) {
-        try {
-            return getTenant(tenantName);
-        } catch (TenantNotFoundException e) {
-            return s_defaultTenant;
-        }
-    }
+	public static IStorefrontTenant getTenantOrDefault(String tenantName) {
+		try {
+			return getTenant(tenantName);
+		} catch (TenantNotFoundException e) {
+			return s_defaultTenant;
+		}
+	}
 
-    public static IStorefrontTenant getTenant(String tenantName) {
-        if (StringUtils.isEmpty(tenantName)) {
-            return s_defaultTenant;
-        }
+	public static IStorefrontTenant getTenant(String tenantName) {
+		if (StringUtils.isEmpty(tenantName)) {
+			return s_defaultTenant;
+		}
 
-        IStorefrontTenant tenant = s_tenantMap.get(tenantName);
-        if (tenant == null) {
-            throw new TenantNotFoundException(tenantName);
-        }
+		IStorefrontTenant tenant = s_tenantMap.get(tenantName);
+		if (tenant == null) {
+			throw new TenantNotFoundException(tenantName);
+		}
 
-        return tenant;
-    }
+		return tenant;
+	}
 
-    public static List<IStorefrontTenant> getAllTenants() {
-        synchronized (s_tenantMap) {
-            return new ArrayList<IStorefrontTenant>(s_tenantMap.values());
-        }
-    }
-    
-    public static boolean isDefaultTenant(IStorefrontTenant tenant) {
-        return tenant == s_defaultTenant;
-    }
+	public static List<IStorefrontTenant> getAllTenants() {
+		synchronized (s_tenantMap) {
+			return new ArrayList<IStorefrontTenant>(s_tenantMap.values());
+		}
+	}
 
-    public static IStorefrontTenant createTenant(String tenantName) {
-        synchronized (s_tenantMap) {
-            if (s_tenantMap.containsKey(tenantName)) {
-                throw new DataValidationException("Tenant \"" + tenantName + "\" already exists");
-            }
+	public static boolean isDefaultTenant(IStorefrontTenant tenant) {
+		return tenant == s_defaultTenant;
+	}
 
-            if (!Pattern.matches("^[0-9A-Za-z\\-]+$", tenantName)) {
-                throw new DataValidationException("Tenant name can contain only letters, numbers, and dashes.");
-            }
+	public static IStorefrontTenant createTenant(String tenantName) {
+		synchronized (s_tenantMap) {
+			if (s_tenantMap.containsKey(tenantName)) {
+				throw new DataValidationException("Tenant \"" + tenantName + "\" already exists");
+			}
 
-            // Configure app instance
-            AppInstance tenantApp = new AppInstance(s_defaultAppInstance.getRegion(), tenantName, true);
-            tenantApp.setUrl(s_defaultAppInstance.getUrl());
+			if (!Pattern.matches("^[0-9A-Za-z\\-]+$", tenantName)) {
+				throw new DataValidationException("Tenant name can contain only letters, numbers, and dashes.");
+			}
 
-            // Configure DB connection info
-            DbConnInfo dbConnInfo = s_defaultTenant.getDbConnInfo();
-            Matcher dbNameMatcher = Pattern.compile("jdbc:com.nuodb://([^/]+)/(.+)$").matcher(dbConnInfo.getUrl());
-            if (!dbNameMatcher.matches()) {
-                throw new DataValidationException("Unable to build database URL");
-            }
-            dbConnInfo.setUrl("jdbc:com.nuodb://" + dbNameMatcher.group(1) + "/" + tenantName);
+			// Configure app instance
+			AppInstance tenantApp = new AppInstance(s_defaultAppInstance.getRegion(), tenantName, true);
+			tenantApp.setUrl(s_defaultAppInstance.getUrl());
 
-            // Build and start tenant
-            StorefrontTenant tenant = new StorefrontTenant(tenantApp);
-            tenant.setDbConnInfo(dbConnInfo);
+			// Configure DB connection info
+			DbConnInfo dbConnInfo = s_defaultTenant.getDbConnInfo();
+			Matcher dbNameMatcher = Pattern.compile("jdbc:com.nuodb://([^/]+)/(.+)$").matcher(dbConnInfo.getUrl());
+			if (!dbNameMatcher.matches()) {
+				throw new DataValidationException("Unable to build database URL");
+			}
+			dbConnInfo.setUrl("jdbc:com.nuodb://" + dbNameMatcher.group(1) + "/" + tenantName);
 
-            s_tenantMap.put(tenantName, tenant);
-            tenant.startUp();
-            return tenant;
-        }
-    }
+			// Build and start tenant
+			StorefrontTenant tenant = new StorefrontTenant(tenantApp);
+			tenant.setDbConnInfo(dbConnInfo);
 
-    public static void destroyTenant(String tenantName) {
-        IStorefrontTenant tenant;
+			s_tenantMap.put(tenantName, tenant);
+			tenant.startUp();
+			return tenant;
+		}
+	}
 
-        synchronized (s_tenantMap) {
-            tenant = s_tenantMap.get(tenantName);
-            if (tenant == null) {
-                throw new DataValidationException("Tenant \"" + tenantName + "\" does not exist");
-            }
-            if (isDefaultTenant(tenant)) {
-                throw new DataValidationException("Cannot remove default tenant");
-            }
-            s_tenantMap.remove(tenantName);
-        }
+	public static IStorefrontTenant createTenant(String tenantName, Map<String, String> dbSettings) {
+		synchronized (s_tenantMap) {
+			if (s_tenantMap.containsKey(tenantName)) {
+				throw new DataValidationException("Tenant \"" + tenantName + "\" already exists");
+			}
 
-        tenant.shutDown();
-    }
+			if (!Pattern.matches("^[0-9A-Za-z\\-]+$", tenantName)) {
+				throw new DataValidationException("Tenant name can contain only letters, numbers, and dashes.");
+			}
+
+			// Configure app instance
+			AppInstance tenantApp = new AppInstance(s_defaultAppInstance.getRegion(), tenantName, true);
+			tenantApp.setUrl(s_defaultAppInstance.getUrl());
+
+			// Build and start tenant
+			StorefrontTenant tenant = new StorefrontTenant(tenantApp, dbSettings.get("dbName"),
+					dbSettings.get("dbUser"), dbSettings.get("dbPassword"), dbSettings.get("dbOptions"));
+
+			s_tenantMap.put(tenantName, tenant);
+			tenant.startUp();
+			return tenant;
+		}
+	}
+
+	public static void destroyTenant(String tenantName) {
+		IStorefrontTenant tenant;
+
+		synchronized (s_tenantMap) {
+			tenant = s_tenantMap.get(tenantName);
+			if (tenant == null) {
+				throw new DataValidationException("Tenant \"" + tenantName + "\" does not exist");
+			}
+			if (isDefaultTenant(tenant)) {
+				throw new DataValidationException("Cannot remove default tenant");
+			}
+			s_tenantMap.remove(tenantName);
+		}
+
+		tenant.shutDown();
+	}
 }
