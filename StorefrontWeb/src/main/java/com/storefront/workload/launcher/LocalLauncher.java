08b0dea50d0fb8566cc2665c7940868208df494f
Kevin White <kwhite@nuodb.com>
2017-06-16 11:03:00 -0400

    [lambda-launcher] Updated local launcher to have standalone functionality

--- StorefrontWeb/src/main/java/com/storefront/workload/launcher/LocalLauncher.java
+++ StorefrontWeb/src/main/java/com/storefront/workload/launcher/LocalLauncher.java
@@ -6,10 +6,15 @@ import java.util.Scanner;
 
 
 public class LocalLauncher implements UserLauncher {
+	
+	private Map<String, String> appOptions;
+	private Map<String, String> dbOptions;
 
 	public void launchUser(Map<String, String> workloadOptions, int count) throws Exception {
 		String workloadOptionsString = buildWorkloadOptionsString(workloadOptions);
-		Process process = Runtime.getRuntime().exec("java -jar /Users/kwhite/interactive-demo/StorefrontUser/target/StorefrontUser.jar " + workloadOptionsString);
+		String dbOptionsString = buildDbOptionsString();
+		String appOptionsString = buildAppOptionsString();
+		Process process = Runtime.getRuntime().exec("java -jar /Users/kwhite/interactive-demo/StorefrontUser/target/StorefrontUser.jar " + dbOptionsString + " " + workloadOptionsString + " " + appOptionsString);
 		Scanner outputScanner = new Scanner(process.getInputStream());
 		Scanner errorScanner = new Scanner(process.getErrorStream());
 		process.waitFor();
@@ -34,5 +39,39 @@ public class LocalLauncher implements UserLauncher {
 			workloadOptionsList.add(optionsBuffer.toString());
 		}
 		return String.join(" ", workloadOptionsList);
+	}
+	
+	private String buildDbOptionsString() {
+		 StringBuffer optionsBuffer = new StringBuffer();
+		 optionsBuffer.append("db.url=");
+		 optionsBuffer.append(getDbOptions().get("db.url"));
+		 optionsBuffer.append(" ");
+		 optionsBuffer.append("db.user=");
+		 optionsBuffer.append(getDbOptions().get("db.user"));
+		 optionsBuffer.append(" ");
+		 optionsBuffer.append("db.password=");
+		 optionsBuffer.append(getDbOptions().get("db.password"));
+		 return optionsBuffer.toString();
+	}
+	
+	private String buildAppOptionsString() {
+		 StringBuffer optionsBuffer = new StringBuffer();
+		 optionsBuffer.append("app.host=");
+		 optionsBuffer.append(getAppOptions().get("app.host"));
+		 return optionsBuffer.toString();
+	}
+
+	public Map<String, String> getDbOptions() {
+		return dbOptions;
+	}
+	public void setDbOptions(Map<String, String> dbOptions) {
+		this.dbOptions = dbOptions;
+	}
+
+	public Map<String, String> getAppOptions() {
+		return appOptions;
+	}
+	public void setAppOptions(Map<String, String> appOptions) {
+		this.appOptions = appOptions;
 	}
 }
