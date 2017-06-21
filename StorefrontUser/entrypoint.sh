2086284740861d09ba3ca222465cd2eefe474aac
ben higgins <bhiggins@nuodb.com>
2017-06-21 11:59:57 -0400

    missed quotes

--- StorefrontUser/entrypoint.sh
+++ StorefrontUser/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-args="workload.multi_browse_and_review.users=$ARG_workloadmulti_browse_and_reviewusers db.url=$ARG_dburl workload.multi_shop.users=$ARG_workloadmulti_shopusers workload.admin_run_report.users=$ARG_workloadadmin_run_reportusers' 'app.host=$ARG_apphost workload.multi_browse.users=$ARG_workloadmulti_browseusers db.password=$ARG_dbpassword db.user=$ARG_dbuser"
+args="workload.multi_browse_and_review.users=$ARG_workloadmulti_browse_and_reviewusers db.url=$ARG_dburl workload.multi_shop.users=$ARG_workloadmulti_shopusers workload.admin_run_report.users=$ARG_workloadadmin_run_reportusers app.host=$ARG_apphost workload.multi_browse.users=$ARG_workloadmulti_browseusers db.password=$ARG_dbpassword db.user=$ARG_dbuser"
 
 echo "java -jar /usr/bin/StorefrontUser.jar $args"
 
